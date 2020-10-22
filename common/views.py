0eab082dccaecc8a6d1da24d7de219a4cac7fd98
dianaperez25 <dianacperezrivera@gmail.com>
2020-10-21 20:00:01 -0700

    delete changes re values page

--- common/views.py
+++ common/views.py
@@ -88,7 +88,3 @@ def data_page(request):
 # directions/contact
 def directions_page(request):
     return render(request, 'common/directions_contact.html')
-
-# values statement
-def values_page(request):
-    return render(request, 'common/values.html')
